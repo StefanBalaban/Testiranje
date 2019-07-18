d9f5110c4e377e1ff8cd921693618a668f091a50
Stefan Balaban <stefanbalaban97@gmail.com>
2019-07-18 21:31:39 +0200

    Testiranje kolekcija

---
+++ Konzola.UnitTests/OsnoveUnitTestiranja.Tests/KolekcijaTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Konzola.OsnoveUnitTestiranja;
+using System.Linq;
+
+namespace Konzola.UnitTests.OsnoveUnitTestiranja.Tests
+{
+    [TestFixture]
+    class KolekcijaTests
+    {
+        [Test]
+        public void Kolekcija_Pozvana_VratiKolekcijuDoVelicine()
+        {
+            var kolekcija = new Kolekcija();
+
+            var result = kolekcija.KolekcijaNeparnihIntegera(5);
+
+            Assert.That(result, Is.EquivalentTo(new int[] { 1, 3}));
+            Assert.That(result.Count(), Is.EqualTo(2));
+            Assert.That(result, Is.Ordered);
+            Assert.That(result, Is.Unique);
+            Assert.That(result, Is.Not.Null);
+        }
+    }
+}
---
+++ Konzola/OsnoveUnitTestiranja/Kolekcija.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Konzola.OsnoveUnitTestiranja
+{
+    public class Kolekcija
+    {
+        public IEnumerable<int> KolekcijaNeparnihIntegera(int velicina)
+        {
+            for (int i = 0; i < velicina; i++)
+                if (i % 2 != 0)
+                    yield return i;            
+        }
+    }
+}
